0265ead11a237bdd0f6dd41c1a3f06566eef2bed
Ricardo <ricarditomontserrat@gmail.com>
2021-03-17 22:44:13 +0100

    LGA-2740 Fix ChangeKarma method outside of if()

--- Assets/Scripts/Karma System/KarmaController.cs
+++ Assets/Scripts/Karma System/KarmaController.cs
@@ -1,37 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class KarmaController : MonoBehaviour
+namespace Karma_System
 {
-   public  int karma = 50;
-   public  Slider karmabar;
-
-    void Start()
+    public class KarmaController : MonoBehaviour
     {
-        karmabar.value = karma;
-    }
+        public int karma = 50;
+        public Slider karmaBar;
 
-    //Only for test if works after implemented.
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.X))
+        private void Start() => karmaBar.value = karma;
+
+        //Only for test if works after implemented.
+        private void Update()
         {
-            ChangeKarma(-1);
+            if (Input.GetKeyDown(KeyCode.X)) ChangeKarma(-1);
+            if (Input.GetKeyDown(KeyCode.Z)) ChangeKarma(+1);
         }
-        if (Input.GetKeyDown(KeyCode.Z))
-            ChangeKarma(+1);
-        {
 
-        }
+        public void ChangeKarma(int increment) => karmaBar.value = karma += increment;
     }
-
-    public void ChangeKarma(int karmanro)
-    {
-        karma += karmanro;
-        karmabar.value = karma;
-    }
-
-   
 }
