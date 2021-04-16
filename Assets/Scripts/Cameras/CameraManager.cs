d6705111808cb945cbb9ef2a0fd27dd588d5d9fc
Ricardo <ricarditomontserrat@gmail.com>
2021-04-16 18:07:21 +0200

    LGA-3343 Added map camera animation, fixed input of show gui, added daylight cycle

--- Assets/Scripts/Cameras/CameraManager.cs
+++ Assets/Scripts/Cameras/CameraManager.cs
@@ -1,4 +1,5 @@
 using Cinemachine;
+using Managers;
 using Player;
 using Plugins.Tools;
 using UnityEngine;
@@ -8,11 +9,14 @@ namespace Cameras
     public class CameraManager : Singleton<CameraManager>
     {
         public CinemachineFreeLook playerCamera;
+        public CinemachineVirtualCamera mapCamera;
 
         public Transform playerTransform;
 
         private const int MIN_CAMERA_ZOOM = 20;
         private const int MAX_CAMERA_ZOOM = 95;
+
+        private bool m_IsOnMap;
 
         protected void Start() => LookForPlayerPosition();
 
@@ -28,6 +32,20 @@ namespace Cameras
             {
                 playerCamera.m_Lens.FieldOfView = Mathf.Lerp(playerCamera.m_Lens.FieldOfView, MAX_CAMERA_ZOOM, PlayerInput.Instance.zoomSpeed);
             }
+
+            if (!PlayerInput.Instance.Map || GameManager.Instance.GameIsPaused) return;
+
+            if (m_IsOnMap)
+            {
+                playerCamera.Priority = 1;
+                mapCamera.Priority = 0;
+            }
+            else
+            {
+                playerCamera.Priority = 0;
+                mapCamera.Priority = 1;
+            }
+            PlayerController.Instance.BlockMovement(m_IsOnMap = !m_IsOnMap);
         }
 
         private void LookForPlayerPosition()
