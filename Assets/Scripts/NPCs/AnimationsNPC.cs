02af78ec41e59df15e75890633b3c0ad5041ef1c
Javier <javier.romero.7e3@itb.cat>
2021-04-19 18:14:15 +0200

    LGA-3140 Get and set bool InCombat for running

--- Assets/Scripts/NPCs/AnimationsNPC.cs
+++ Assets/Scripts/NPCs/AnimationsNPC.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Combat;
 
 namespace NPCs
 {
@@ -10,13 +11,15 @@ namespace NPCs
         private Animator m_Animator;
         private Transform m_Player;
         private NavMeshAgent m_Agent;
+        private Enemy m_Enemy;
 
         private readonly int SPEED_ANIMATION_HASH = Animator.StringToHash("Speed");
 
         protected void Awake()
         {
             m_Animator = GetComponent<Animator>();
-            m_Agent = GetComponent<NavMeshAgent>();
+            m_Agent = GetComponent<NavMeshAgent>(); 
+            m_Enemy = GetComponent<Enemy>();          
         }
 
         private void FixedUpdate()
@@ -32,7 +35,7 @@ namespace NPCs
             }
             else
             {
-                m_Animator.SetFloat(SPEED_ANIMATION_HASH, 0.5f, 0.15f, Time.deltaTime);
+                m_Animator.SetFloat(SPEED_ANIMATION_HASH, m_Enemy.InCombat ? 1f : 0.5f, 0.15f, Time.deltaTime);
             }
         }
     }
