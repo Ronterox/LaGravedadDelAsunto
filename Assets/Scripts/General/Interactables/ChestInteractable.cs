387e460abdcbf0d4498c24e63f4a5818ea3791ff
Ricardo <ricarditomontserrat@gmail.com>
2021-04-29 18:05:11 +0200

    LGA-5991 Added vendor with script, and chest script

---
+++ Assets/Scripts/General/Interactables/ChestInteractable.cs
@@ -0,0 +1,53 @@
+using System;
+using General.Utilities;
+using Inventory_System;
+using Managers;
+using Plugins.Persistence;
+using Plugins.Tools;
+using UnityEngine;
+
+namespace General.Interactables
+{
+    public class ChestInteractable : Interactable, IDataPersister
+    {
+        public Item[] items;
+        [Space]
+        public Transform helpMessagePosition;
+        public bool isUnlock;
+
+        public DataSettings dataSettings;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            dataSettings.GenerateId(gameObject);
+        }
+
+        private void OnEnable()
+        {
+            PersistentDataManager.LoadPersistedData(this);
+            gameObject.SetActive(!isUnlock);
+        }
+
+        private void OnDisable() => PersistentDataManager.SavePersistedData(this);
+
+        public override void Interact()
+        {
+            GameManager.Instance.inventory.SpawnItem(items.GetRandom(), transform.position);
+            Action deactivate = () => gameObject.SetActive(false);
+            deactivate.DelayAction(2f);
+        }
+
+        protected override void OnEnterTrigger(Collider other) => GameManager.Instance.dialogueManager.Type("Press \"E\" to interact!", helpMessagePosition.position);
+
+        protected override void OnExitTrigger(Collider other) { }
+
+        public DataSettings GetDataSettings() => dataSettings;
+
+        public void SetDataSettings(ushort dataId, DataSettings.PersistenceType persistenceType) { }
+
+        public Data SaveData() => new Data<bool>(isUnlock);
+
+        public void LoadData(Data data) => isUnlock = ((Data<bool>)data).value;
+    }
+}
